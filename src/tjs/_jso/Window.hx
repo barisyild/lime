b4590fc3d62a17a9b1729c9f602d7d36856d7346
barisyild <25794892+barisyild@users.noreply.github.com>
2026-08-07 22:47:05 +0300

    Add `getFrameElement` method to `Window` interface in `tjs/_jso`

--- src/tjs/_jso/Window.hx
+++ src/tjs/_jso/Window.hx
@@ -8,6 +8,7 @@ extern class Window {
 	function getDocument():HTMLDocument;
 	function getLocation():JSObject;
 	function getScreen():Screen;
+	function getFrameElement():JSObject;
 	function getInnerWidth():Int;
 	function getInnerHeight():Int;
 	function getDevicePixelRatio():Float;
